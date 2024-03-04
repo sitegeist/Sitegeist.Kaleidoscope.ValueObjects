6f447d7a426a63953d2db5bc0a1bf20debed74ac
Christian Pansch <christian.pansch@sitegeist.de>
2024-03-04 15:00:40 +0100

    fix manifest

--- Neos.Ui/plugin/src/manifest.js
+++ Neos.Ui/plugin/src/manifest.js
@@ -1,12 +1,6 @@
 import manifest from '@neos-project/neos-ui-extensibility'
 import { registerAssetWithMetadataEditor } from '@sitegeist/kaleidoscope-valueobjects-asset-with-metadata-editor'
 
-manifest(
-    '@sitegeist/kaleidoscope-valueobjects-plugin',
-    {},
-    (globalRegistry, { store, configuration, routes }) => {
-        const neosContextProperties = { globalRegistry, store, configuration, routes }
-
-        registerAssetWithMetadataEditor(neosContextProperties, editor)
-    }
-)
+manifest('@sitegeist/kaleidoscope-valueobjects-plugin', {}, (globalRegistry) => {
+    registerAssetWithMetadataEditor(globalRegistry)
+})
